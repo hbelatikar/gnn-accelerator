4c2281b9ecb7916ff279b2a393cfc52608697485
hbelatikar <hrishikesh.belatikar@gmail.com>
2022-02-24 20:52:29 -0600

    Promoted @deepas design as top level for submission process. Updated testbench as per new design.

--- rtl/MS1/top_tb.sv
+++ rtl/MS1/top_tb.sv
@@ -1,72 +1,92 @@
 module tb_top();
 
-reg [15:0] x0, x1, x2, x3;
-reg [15:0] w04, w14, w24, w34;
-reg [15:0] w05, w15, w25, w35;
-reg [15:0] w06, w16, w26, w36;
-reg [15:0] w07, w17, w27, w37;
-reg [15:0] w48, w58, w68, w78;
-reg [15:0] w49, w59, w69, w79;
+reg [4:0] x0, x1, x2, x3;
+reg [4:0] w04, w14, w24, w34;
+reg [4:0] w05, w15, w25, w35;
+reg [4:0] w06, w16, w26, w36;
+reg [4:0] w07, w17, w27, w37;
+reg [4:0] w48, w58, w68, w78;
+reg [4:0] w49, w59, w69, w79;
 
 reg clk;
 
-wire [15:0] out0, out1;
-wire out11_ready, out10_ready ;
+wire [16:0] out0, out1;
+wire out10_ready, out11_ready;
 
 reg in_ready;
 // Top module
 // Instantiation of top module
-// Please replace the instantiation with the top module of your gate level model 
+// Please replace the instantiation with the top module of your gate level model
+// Look for 'test failed' in the message. If there is no such message then your output matches the golden outputs. 
 
 
-nn_node DUT0(   .in0(x0), .in1(x1), .in2(x2), .in3(x3), 
-                .w04(w04), .w14(w14), .w24(w24), .w34(w34), 
-                .w05(w05), .w15(w15), .w25(w25), .w35(w35),
-                .w06(w06), .w16(w16), .w26(w26), .w36(w36),
-                .w07(w07), .w17(w17), .w27(w27), .w37(w37),
-                .w48(w48), .w58(w58), .w68(w68), .w78(w78),
-                .w49(w49), .w59(w59), .w69(w69), .w79(w79),
-                .out0(out0), .out1(out1),
-                .in_ready(in_ready), .out_ready(out11_ready),
-                .clk(clk));
+top top(.x0(x0), .x1(x1), .x2(x2), .x3(x3), 
+        .w04(w04), .w14(w14), .w24(w24), .w34(w34), 
+        .w05(w05), .w15(w15), .w25(w25), .w35(w35),
+        .w06(w06), .w16(w16), .w26(w26), .w36(w36),
+        .w07(w07), .w17(w17), .w27(w27), .w37(w37),
+        .w48(w48), .w58(w58), .w68(w68), .w78(w78),
+        .w49(w49), .w59(w59), .w69(w69), .w79(w79),
+        .out0(out0), .out1(out1),
+        .in_ready(in_ready), .out10_ready(out10_ready), .out11_ready(out11_ready),
+        .clk(clk));
 
 initial begin
 
     clk = 0;
     in_ready = 1; 
     
-    x0 = 15'b0100;
-    x1 = 15'b0010;
-    x2 = 15'b0100;
-    x3 = 15'b0001;
-    w04 = 15'b0011;
-    w14 = 15'b0010;
-    w24 = 15'b1101;
-    w34 = 15'b0;
-    w05 = 15'b0;
-    w15 = 15'b0;
-    w25 = 15'b0;
-    w35 = 15'b1110;
-    w06 = 15'b0011;
-    w16 = 15'b0110;
-    w26 = 15'b0000;
-    w36 = 15'b1111;
-    w07 = 15'b1001;
-    w17 = 15'b0;
-    w27 = 15'b1111;
-    w37 = 15'b0;
-    w48 = 15'b0;
-    w58 = 15'b0;
-    w68 = 15'b0011;
-    w78 = 15'b1011;
-    w49 = 15'b1100;
-    w59 = 15'b0;
-    w69 = 15'b0;
-    w79 = 15'b0110;
+    x0 = 5'b00100;
+    x1 = 5'b00010;
+    x2 = 5'b00100;
+    x3 = 5'b00001;
+    
+    w04 = 5'b00011;
+    w14 = 5'b00010;
+    w24 = 5'b01101;
+    w34 = 5'b11010;
+    w05 = 5'b10111;
+    w15 = 5'b00001;
+    w25 = 5'b11100;
+    w35 = 5'b01110;
+    w06 = 5'b00011;
+    w16 = 5'b00110;
+    w26 = 5'b10001;
+    w36 = 5'b01111;
+    w07 = 5'b01001;
+    w17 = 5'b10110;
+    w27 = 5'b01111;
+    w37 = 5'b10110;
+    w48 = 5'b00000;
+    w58 = 5'b11111;
+    w68 = 5'b00011;
+    w78 = 5'b10101;
+    w49 = 5'b10100;
+    w59 = 5'b10001;
+    w69 = 5'b10001;
+    w79 = 5'b00110;
+
+
+    #40
+    if (out0 == -17'd726)
+        $display("-----------out0 is correct-----------------");
+    else
+        $display("-----------out0 is incorrect-----------");
+
+    if (out1 == -17'd348)
+        $display("-----------out1 is correct-----------");
+    else
+        $display("-----------out1 is incorrect-----------");
+
+
+    if (out0 == -17'd726 && out1 == -17'd348)
+        $display("*********** ALL TESTS PASSED *********");
+    else
+        $display("*********** SOME TEST(S) FAILED *********");
+
 
 end
 
-assign out10_ready = out11_ready;
 
 always
     #1 clk = !clk;
